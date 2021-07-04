7fa4ce2d8e811df7b88a383c74847567c0cefc61
raque <raquel.f.peralta@hotmail.com>
2021-07-04 20:30:23 +0100

    added the multiplayer version, and transformed them into functions to simplify the game app

--- Hangman.py
+++ Hangman.py
@@ -1,95 +1,207 @@
-word = ["B", "O", "L", "O"]
-word_progress = ["_", "_", "_", "_"]
-errors = []
-# hangman progress:
-hangman_list = [
-    """
+def hangman_solo():
+
+    # setup:
+    word = ["B", "O", "L", "O"]
+    word_progress = ["_", "_", "_", "_"]
+    errors = []
+    # hangman progress:
+    hangman_list = [
+        """
+        
+        
+    """,
+        """
+         o
     
     
-""",
-    """
-     o
+    """,
+        """
+         o
+         |
+    
+    """,
+        """
+         o
+        (|
+    
+    """,
+        """
+         o
+        (|)
+    
+    """,
+        """
+         o
+        (|)
+        | 
+    """,
+        """
+         o
+        (|)
+        | |
+    """]
+
+    # logic:
+
+    while word_progress != word and errors.__len__() < 6:
+
+        # interface:
+
+        hangman = hangman_list[errors.__len__()]    # to check how to draw the hangman at each phase
+
+        print("Word progress:")
+        print(word_progress)
+        print("Errors:")
+        print(errors)
+        print(hangman)
+        letter = input("Choose a letter: ").upper()
+
+        # validating the chosen letter:
+
+        if letter in errors or letter in word_progress:  # to check if the letter is repeated
+            print("")
+            print("Repeated letter. Try again.")
+
+        elif letter in word:  # to check if the letter is in the word
+            print("")
+
+            # to replace all of the correct letters:
+            i = 0
+            while i < (len(word)):
+                if word[i] == letter:
+                    word_progress[i] = letter
+                    i = i + 1
+                else:
+                    i = i + 1
+
+        else:   # if it's not repeated nor correct, it's an error
+            print("")
+            print("Nope.")
+            errors.append(letter)
+            errors.sort()
+
+    # when the game loop ends:
+
+    if word_progress == word:   # to check if player won
+        print(word_progress)
+        print(hangman)
+        print("Congratulations! You win!")
+        input()
+
+    else:   # to check if the player lost their lives
+        print("You lose!")
+        print(hangman)
+        print("Correct word:")
+        print(word)
+        print("Errors:")
+        print(errors)
+        input()
 
 
-""",
-    """
-     o
-     |
+def hangman_multiplayer():
+    # setup:
+    word_input = input("Player 1, insert the word that you want the other players to guess. ")
+    word = []
+    word_progress = []
+    errors = []
 
-""",
-    """
-     o
-    (|
+    # create the lists:
+    i = 0
+    while i < len(word_input):
+        word.append(word_input[i])
+        word_progress.append("_")
+        i = i + 1
 
-""",
-    """
-     o
-    (|)
+    # hangman progress:
+    hangman_list = [
+        """
 
-""",
-    """
-     o
-    (|)
-    | 
-""",
-    """
-     o
-    (|)
-    | |
-"""]
 
-# logic:
+    """,
+        """
+         o
 
-while word_progress != word and errors.__len__() < 6:
 
-    # interface:
+    """,
+        """
+         o
+         |
 
-    hangman = hangman_list[errors.__len__()]    # to check how to draw the hangman at each fase
+    """,
+        """
+         o
+        (|
 
-    print("Word progress:")
-    print(word_progress)
-    print("Errors:")
-    print(errors)
-    print(hangman)
-    letter = input("Choose a letter: ").upper()
+    """,
+        """
+         o
+        (|)
 
-    # validating the chosen letter:
+    """,
+        """
+         o
+        (|)
+        | 
+    """,
+        """
+         o
+        (|)
+        | |
+    """]
 
-    if letter in errors or letter in word_progress:  # to check if the letter is repeated
-        print("")
-        print("Repeated letter. Try again.")
+    # logic:
 
-    elif letter in word:  # to check if the letter is in the word
-        print("")
+    while word_progress != word and errors.__len__() < 6:
 
-        # to replace all of the correct letters:
-        i = 0
-        while i < (len(word)):
-            if word[i] == letter:
-                word_progress[i] = letter
-                i = i + 1
-            else:
-                i = i + 1
+        # interface:
 
-    else:   # if it's not repeated nor correct, it's an error
-        print("")
-        print("Nope.")
-        errors.append(letter)
-        errors.sort()
+        hangman = hangman_list[errors.__len__()]  # to check how to draw the hangman at each phase
 
-# when the game loop ends:
+        print("Word progress:")
+        print(word_progress)
+        print("Errors:")
+        print(errors)
+        print(hangman)
+        letter = input("Choose a letter: ").upper()
 
-if word_progress == word:   # to check if player won
-    print(word_progress)
-    print(hangman)
-    print("Congratulations! You win!")
-    input()
+        # validating the chosen letter:
 
-else:   # to check if the player lost their lives
-    print("You lose!")
-    print(hangman)
-    print("Correct word:")
-    print(word)
-    print("Errors:")
-    print(errors)
-    input()
+        if letter in errors or letter in word_progress:  # to check if the letter is repeated
+            print("")
+            print("Repeated letter. Try again.")
+
+        elif letter in word:  # to check if the letter is in the word
+            print("")
+
+            # to replace all of the correct letters:
+            i = 0
+            while i < (len(word)):
+                if word[i] == letter:
+                    word_progress[i] = letter
+                    i = i + 1
+                else:
+                    i = i + 1
+
+        else:  # if it's not repeated nor correct, it's an error
+            print("")
+            print("Nope.")
+            errors.append(letter)
+            errors.sort()
+
+    # when the game loop ends:
+
+    if word_progress == word:  # to check if player won
+        print(word_progress)
+        print(hangman)
+        print("Congratulations! You win!")
+        input()
+
+    else:  # to check if the player lost their lives
+        print("You lose!")
+        print(hangman)
+        print("Correct word:")
+        print(word)
+        print("Errors:")
+        print(errors)
+        input()
+
