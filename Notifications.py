21ad4bd0c21f230d2aad89fb1defa469a2454331
raque <raquel.f.peralta@hotmail.com>
2021-07-17 18:35:17 +0100

    initial commit

---
+++ Notifications.py
@@ -0,0 +1,17 @@
+from win10toast import ToastNotifier
+import time
+
+
+def notification(title, message):
+    toaster = ToastNotifier()
+    toaster.show_toast(
+        title,
+        message,
+        icon_path=None,
+        duration=7)
+
+
+if __name__ == "__main__":
+    while True:
+        notification("Break Time", "It's time to have a break.")
+        time.sleep(1200)
